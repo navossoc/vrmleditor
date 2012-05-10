dd75006c109cb31c9cc1955ae5f88b6a872f198b
navossoc <navossoc@c7669036-a199-da45-88c4-984b30b367fa>
2012-05-10 17:53:08 +0000

    Adicionado Shape Cylinder (falta otimizar o código do mesh).

--- trunk/src/shape/geometry/Cylinder.java
+++ trunk/src/shape/geometry/Cylinder.java
@@ -1,5 +1,10 @@
 package shape.geometry;
 
+import com.badlogic.gdx.graphics.GL10;
+import com.badlogic.gdx.graphics.Mesh;
+import com.badlogic.gdx.graphics.VertexAttribute;
+import com.badlogic.gdx.graphics.VertexAttributes;
+import com.badlogic.gdx.math.MathUtils;
 import shape.Shape;
 
 public class Cylinder extends Shape {
@@ -10,18 +15,71 @@ public class Cylinder extends Shape {
     public Cylinder(float radius, float height) {
         this.height = height;
         this.radius = radius;
+
+        primitiveType = GL10.GL_TRIANGLES;
+        calculateCylinder();
     }
 
-    @Override
-    public void draw() {
-        super.draw();
-/*
-        int x = (int) translation.x;
-        int y = (int) translation.y;
-        int w = (int) radius;
-        int h = (int) height;
-*/
+    private void calculateCylinder() {
+        short slices = (short) (6 * Math.cbrt(radius));
 
+        mesh = new Mesh(true, (slices + 1) * 2, slices * 12, new VertexAttribute(VertexAttributes.Usage.Position, 3, "a_position"));
+
+        float[] vertices = new float[(slices + 1) * 6];
+        float angle = 360f / slices;
+
+        int i = 0;
+        for (int s = 0; s < slices; i += 3, s++) {
+            vertices[i++] = vertices[i + 2] = radius * MathUtils.cosDeg(s * angle);
+            vertices[i++] = -height / 2;
+            vertices[i + 2] = height / 2;
+            vertices[i++] = vertices[i + 2] = radius * MathUtils.sinDeg(s * angle);
+        }
+        vertices[i++] = vertices[i + 2] = 0;
+        vertices[i++] = -height / 2;
+        vertices[i + 2] = height / 2;
+        vertices[i++] = vertices[i + 2] = 0;
+
+        short[] indices = new short[slices * 12];
+
+        short p = 0;
+        for (i = 0; i < indices.length - 12;) {
+            //
+            indices[i++] = p;
+            indices[i++] = (short) (p + 2);
+            indices[i++] = (short) (slices * 2);
+            //
+            indices[i++] = (short) (p + 1);
+            indices[i++] = (short) (p + 3);
+            indices[i++] = (short) (slices * 2 + 1);
+            //
+            indices[i++] = p;
+            indices[i++] = (short) (p + 1);
+            indices[i++] = (short) (p + 2);
+            //
+            indices[i++] = (short) (p + 1);
+            indices[i++] = (short) (p + 2);
+            indices[i++] = (short) (p + 3);
+            p += 2;
+        }
+        indices[i++] = p;
+        indices[i++] = 0;
+        indices[i++] = (short) (slices * 2);
+
+        indices[i++] = (short) (p + 1);
+        indices[i++] = 1;
+        indices[i++] = (short) (slices * 2 + 1);
+
+        indices[i++] = 0;
+        indices[i++] = p;
+        indices[i++] = (short) (p + 1);
+
+        indices[i++] = 0;
+        indices[i++] = 1;
+        indices[i++] = (short) (p + 1);
+
+        mesh.setVertices(vertices);
+        mesh.setIndices(indices);
     }
 
     @Override
