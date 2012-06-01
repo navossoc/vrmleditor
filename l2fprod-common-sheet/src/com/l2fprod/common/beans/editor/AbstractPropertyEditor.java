8a4c778ed60d7e5207682557432a66926719192d
navossoc <navossoc@c7669036-a199-da45-88c4-984b30b367fa>
2012-06-01 22:00:20 +0000

    Adicionado projeto do Property Sheet do l2fprod do fred. Foram feitas pequenas modificações apenas para importar o projeto no NetBeans, não há nenhuma mudança de código (ainda).

---
+++ l2fprod-common-sheet/src/com/l2fprod/common/beans/editor/AbstractPropertyEditor.java
@@ -0,0 +1,96 @@
+/**
+ * L2FProd.com Common Components 7.3+ License.
+ *
+ * Copyright 2007 L2FProd.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.l2fprod.common.beans.editor;
+
+import java.awt.Component;
+import java.awt.Graphics;
+import java.awt.Rectangle;
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
+import java.beans.PropertyEditor;
+
+/**
+ * AbstractPropertyEditor. <br>
+ *
+ */
+public class AbstractPropertyEditor implements PropertyEditor {
+
+    protected Component editor;
+    private PropertyChangeSupport listeners = new PropertyChangeSupport(this);
+
+    @Override
+    public boolean isPaintable() {
+        return false;
+    }
+
+    @Override
+    public boolean supportsCustomEditor() {
+        return false;
+    }
+
+    @Override
+    public Component getCustomEditor() {
+        return editor;
+    }
+
+    @Override
+    public void addPropertyChangeListener(PropertyChangeListener listener) {
+        listeners.addPropertyChangeListener(listener);
+    }
+
+    @Override
+    public void removePropertyChangeListener(PropertyChangeListener listener) {
+        listeners.removePropertyChangeListener(listener);
+    }
+
+    protected void firePropertyChange(Object oldValue, Object newValue) {
+        listeners.firePropertyChange("value", oldValue, newValue);
+    }
+
+    @Override
+    public Object getValue() {
+        return null;
+    }
+
+    @Override
+    public void setValue(Object value) {
+    }
+
+    @Override
+    public String getAsText() {
+        return null;
+    }
+
+    @Override
+    public String getJavaInitializationString() {
+        return null;
+    }
+
+    @Override
+    public String[] getTags() {
+        return null;
+    }
+
+    @Override
+    public void setAsText(String text) throws IllegalArgumentException {
+    }
+
+    @Override
+    public void paintValue(Graphics gfx, Rectangle box) {
+    }
+}
